70e4ba08b8f63c8e11761d72dc5bb09cb752bfa6
Paulo Villa <prcvilla@gmail.com>
2017-07-20 10:52:08 -0300

    basic make now takes IRUNS into consideration. make sim-run now can be ran with DOFILE=file.do at the end

--- 1-codigo/basic/main.c
+++ 1-codigo/basic/main.c
@@ -5,12 +5,16 @@
 #define FIN 7
 #define ERR 8
 
+#ifndef IRUNS
+#define IRUNS 500
+#endif
+
 volatile int x,a=9,b=8,c=4,d=5,i=0;
 
 int main(){
 	GPIO_SET_OUTPUTS;
 	GPIO_WRITE(0x0);
-	while(i<500){
+	while(i<IRUNS){
 		GPIO_SETPIN(CALC);
 		x=(a+b)-(c+d);
 		if(x!=8){GPIO_SETPIN(ERR);}
